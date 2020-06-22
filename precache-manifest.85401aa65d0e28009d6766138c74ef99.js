18c2e1f19c54bb2d8d6b546306d6795071052c4c
ksennes <ksenneste1@gmail.com>
2020-06-22 10:50:15 +0300

    Updates

---
+++ precache-manifest.85401aa65d0e28009d6766138c74ef99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1e23412bc89f50c77f7f2dfa79db64",
+    "url": "/pic-of-the-day/index.html"
+  },
+  {
+    "revision": "6d32bfb2a072426552c6",
+    "url": "/pic-of-the-day/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "eeebaf296c6080b2c75a",
+    "url": "/pic-of-the-day/static/css/main.92a74143.chunk.css"
+  },
+  {
+    "revision": "6d32bfb2a072426552c6",
+    "url": "/pic-of-the-day/static/js/2.93deb21a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/pic-of-the-day/static/js/2.93deb21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeebaf296c6080b2c75a",
+    "url": "/pic-of-the-day/static/js/main.c997fbb3.chunk.js"
+  },
+  {
+    "revision": "ca7fde38a17d6150dd6f",
+    "url": "/pic-of-the-day/static/js/runtime-main.e03bc501.js"
+  }
+]);
